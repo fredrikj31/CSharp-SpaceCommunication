8ed0b2e17558a02117d789e085a6ed12b81150f2
fredrikj31 <fredrik1206@gmail.com>
2021-02-06 15:54:29 +0100

    Added corrupt data and conversion

---
+++ SpaceCommunication/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceCommunication {
+	class Converter {
+		public byte[] convertToBytes(string inputMessage) {
+			return Encoding.ASCII.GetBytes(inputMessage);
+		}
+
+		public byte[] corruptData(byte[] inputBytes) {
+			Random rnd = new Random();
+
+			byte[] data = inputBytes;
+
+			rnd.NextBytes(data);
+
+			return data;
+		}
+
+		public string convertToString(byte[] inputBytes) {
+
+			return Encoding.ASCII.GetString(inputBytes);
+		}
+
+	}
+}
---
+++ SpaceCommunication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceCommunication {
+	class Program {
+		static void Main(string[] args) {
+			Converter converter = new Converter();
+
+			string message = "Hello World";
+
+			byte[] result = converter.convertToBytes(inputMessage: message);
+
+			result = converter.corruptData(result);
+
+			Console.WriteLine(converter.convertToString(result));
+			
+		}
+	}
+}
