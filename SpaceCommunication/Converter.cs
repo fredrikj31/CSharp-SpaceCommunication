aeed3dde2fcc36bb5dfad12ceb46791d41b11493
fredrikj31 <fredrik1206@gmail.com>
2021-02-12 13:41:30 +0100

    Added comments

--- SpaceCommunication/Converter.cs
+++ SpaceCommunication/Converter.cs
@@ -4,56 +4,62 @@ using System.Text;
 
 namespace SpaceCommunication {
 	class Converter {
-		// Opmærksom på 8 og 16 bit.
-
 		public List<string> convertToBytes(string inputMessage) {
+			// Declaring list of characters
 			List<string> result = new List<string>();
-
+			// Splitting string into characters
 			foreach (char c in inputMessage) {
+				// Adding characters to list
 				result.Add(Convert.ToString(c, 2).PadLeft(8, '0'));
 			}
-
+			// Returning the list of bytes in string format
 			return result;
 		}
-
 		public List<string> corruptData(List<string> inputBytes) {
+			// Making a new instance of the random class
 			Random rnd = new Random();
 
+			// Looping 8 times
 			for (int i = 0; i < 8; i++) {
+				// Generate random number between 0 and list of bytes
 				int randomItem = rnd.Next(0, inputBytes.Count);
+				// Generate random number between 0 and 8
 				int randomBytePlace = rnd.Next(0, 8);
-
-
+				// Declaring array of characters with a length of random number
 				char[] charMessage = inputBytes[randomItem].ToCharArray();
-
+				// Checking if the bit is 1
 				if (charMessage[randomBytePlace] == '1') {
+					// Setting the bit to 0
 					charMessage[randomBytePlace] = '0';
+					// Else if the bit is 0
 				} else {
+					// Setting the bit to 1
 					charMessage[randomBytePlace] = '1';
 				}
-
+				// Making a new instance of the StringBuilder class
 				StringBuilder stringByte = new StringBuilder();
-
+				// Appending the characters to a string
 				foreach (var item in charMessage) {
 					stringByte.Append(item);
 				}
+				// Removing the right bit and replacing it with the corrupted bit
 				inputBytes.RemoveAt(randomItem);
 				inputBytes.Insert(randomItem, stringByte.ToString());
 			}
-
+			// Returning the list of bytes in string format
 			return inputBytes;
 		}
 
 		public string convertToString(List<string> inputBytes) {
+			// Making a new instance of the StringBuilder class
 			StringBuilder result = new StringBuilder();
-
+			// Looping through every character in the inputted string
 			foreach (string charByte in inputBytes) {
-				//Console.WriteLine(charByte);
+				// Appending the characters into the string builder
 				result.Append((char)Convert.ToInt32(charByte, 2));
 			}
-
+			// Returning the fully appending string, and parsing it into a string
 			return result.ToString();
 		}
-
 	}
 }
--- SpaceCommunication/Program.cs
+++ SpaceCommunication/Program.cs
@@ -9,11 +9,14 @@ namespace SpaceCommunication {
 
 			string message = "";
 
+			// Asks the user for a message
 			Console.WriteLine("Enter you messages please: ");
 			message = Console.ReadLine();
 
+			// Converts the message into bytes
 			List<string> result = converter.convertToBytes(inputMessage: message);
 
+			// Writing the bytes out in the console
 			Console.WriteLine("----------------------------------- Converted Message to Binary -----------------------------------");
 			foreach (var item in result) {
 				Console.Write(item);
@@ -25,7 +28,9 @@ namespace SpaceCommunication {
 			Console.WriteLine(" ");
 			Console.WriteLine(" ");
 
+			// Corrupt the bytes
 			List<string> corruptData = converter.corruptData(result);
+			// Writing the corrupted bytes out in the console
 			Console.WriteLine("----------------------------------- Corrupted Message to Binary -----------------------------------");
 			foreach (var item in corruptData) {
 				Console.Write(item);
@@ -37,6 +42,7 @@ namespace SpaceCommunication {
 			Console.WriteLine(" ");
 			Console.WriteLine(" ");
 
+			// Printing the corruped message out in the console
 			Console.WriteLine("------------------------------------ Corrupted Message to Text ------------------------------------");
 			Console.WriteLine(converter.convertToString(corruptData));
 			Console.WriteLine("------------------------------------ Corrupted Message to Text ------------------------------------");
